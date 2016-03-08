7f1d2127a11b96d8a858d0d14e6e2fbbff662d11
Daniel Hollenbeck <dan@hollenbecks.com>
2016-03-08 08:15:47 -0600

    work on addValidator() and checkValidator()

--- README.md
+++ README.md
@@ -6,17 +6,21 @@ An event based validation plugin used for client-side validation of html forms.
 
 How is this validator plugin different than other jquery plugin form validators?
 
-###Validations 
+###Validations
 
 Unlike all other form validations this plugin primary focus is the validation rules and not the form inputs. During validations the plugin will loop the defined fields config looking for any inputs which might exist and if and only if the form input exists validate the input's value. All other plugins loop the inputs and validate the form inputs. This plugin loops the validations and could careless if a form input exists or not.
 
-This plugin will validate only what you specify. If you do not define a field to be validated it will be not be validated. Also, if you define a field to be validated it will be validated event if the input is hidden, disabled, or readonly. All other plugins will validate what you don't want them to and expect you figure out how to exclude/ignore these inputs you did not ask be validated from being validated. They will also ignore inputs you explictly asked to be validated with no reasonable way to force the validation to happen. For example, try validating a form which uses jquery-multiselect on it using any of the other validation plugins.  
+This plugin will validate only what you specify. If you do not define a field to be validated it will be not be validated. Also, if you define a field to be validated it will be validated event if the input is hidden, disabled, or readonly. All other plugins will validate what you don't want them to and expect you figure out how to exclude/ignore these inputs you did not ask be validated from being validated. They will also ignore inputs you explictly asked to be validated with no reasonable way to force the validation to happen. For example, try validating a form which uses jquery-multiselect on it using any of the other validation plugins.
 
-###Decoration  
+### Validators
+
+The validator methods should be seperate from the validation plugin. This is again because of seperation of concerns, but also because the validators maybe shared between the frontend and backend (via Node.js). Theis would imply that the validators should not be passed DOM references.
+
+###Decoration
 
 Form and input decorations should not be part of a validation plugin (e.g. seperation of concerns). The validation code should be loosely coupled with the decoration code by DOM events. The validation code triggers events and the decoration code listens to these events and decorates the DOM accordingly. See jquery-decorator.
 
-###Delagated Events 
+###Delagated Events
 
 All other form validation plugins attach keyup events directly to the form inputs to provide live validation to the user. These event listens should be **delegated** to the plugin container (ie normally the form tag) to allow for dynamic changes to the form DOM. Changes to the form DOM are common with most advanced form control plugins, dynamic HTML features.
 
@@ -86,12 +90,11 @@ form.on(['prove.success', 'prove.failure'], function(event, data){
 
 The following events are emitted by the plugin on the form container.
 
-- 'prove.success', {values: values}
-- 'prove.failure', {failures: [{message: '', value: '', element: ''}], success: [{value: '', element: ''}]}
-- 'prove.field.success', {value: value, element: element}
-- 'prove.field.failure', {value: value, element: element, validator: validator}
-- 'prove.field.reset', {...}
-- 'prove.field.initalized', {field data}
+- `valid.form.prove`, {...}
+- `invalid.form.prove`, {...}
+- `valid.field.prove`, {...}
+- `invalid.field.prove`, {...}
+- `reset.field.prove`, {...}
 
 ## Others
 
--- jquery-prove.js
+++ jquery-prove.js
@@ -4,22 +4,50 @@
 !function ($) {
 	"use strict";
 
-	 // Constructor
-	//$.Prove = function(element, options){
+	var _validators = {
+		required: function( param, value, values) {
+
+			console.log('required()', param, value, values);
+
+			return true;
+
+			if (param === false) {
+
+			}
+			// Check if dependency is met
+			if ( !this.depend( param, element ) ) {
+				return "dependency-mismatch";
+			}
+			if ( element.nodeName.toLowerCase() === "select" ) {
+
+				// Could be an array for select-multiple or a string, both are fine this way
+				var val = $( element ).val();
+				return val && val.length > 0;
+			}
+			if ( this.checkable( element ) ) {
+				return this.getLength( value, element ) > 0;
+			}
+			return value.length > 0;
+		},
+
+	};
+
+	// Prove constructor
 	function Prove(form, options) {
 
 		this.$form = $(form);
 
 		this.options = this.mergeOptions($.extend({}, options, this.$form.data()));
 
-		console.log('Prove()', this.options);
+		console.log('Prove()');
 
 		this.setupFields();
 	}
 
-	$.Prove = Prove;
-	$.Prove.prototype.addValidator = function(name, method){
-		this._validators[ name ] = method;
+	//add public method to constructor
+	Prove.addValidator = function(name, method){
+		console.log('existing validators', _validators);
+		_validators[ name ] = method;
 	};
 
 	//$.Prove.prototype.defaults = {
@@ -36,12 +64,6 @@
 
 		constructor: Prove,
 
-		_validators: {},
-
-/*		addValidator: function(name, method){
-			_validators[ name ] = method;
-		},*/
-
 		destroy: function() {
 			console.log('destroy()');
 			//this.$container.remove();
@@ -53,7 +75,7 @@
 
 			this.teardownFields();
 
-			el.trigger('prove.destroyed');
+			el.trigger('destroyed.prove');
 		},
 		setOptions: function(options) {
 			this.options = this.mergeOptions(options);
@@ -95,13 +117,13 @@
 				that.unbindDomEvent(name, field);
 			});
 		},
-		//delagate events on form form for specific field
+		//delegate DOM events to form
 		bindDomEvent: function(name, field){
 
 			var el = this.$form;
 			var events = this.domEvents(name, field);
 			var selector = this.domSelector(name, field);
-			var handler = $.proxy(this.validateFieldHandler, this); //todo: do in constructor?
+			var handler = $.proxy(this.validateFieldHandler, this);
 
 			console.log('bindDomEvents()', events, selector);
 
@@ -129,21 +151,36 @@
 			console.log('validateFieldHandler()', field);
 
 			$.each(validators, function(name, param){
-				that.checkValidator(name, param, value);
+				that.checkValidator(input, name, param, value);
 			});
 		},
-		checkValidator: function(name, param, value){
-			var validator = this._validators[name];
+		checkValidator: function(input, name, param, value){
 
-			// return early with warning
-			if (!validator) return console.warn("Validator '%s' not found. Please use $.Prove.addValidator().", name);
+			console.log('checkValidator()', input, name, param, value);
 
-			console.log('checkValidator()', name, param, value);
+			// setup
+			var validator = $.proxy(_validators[name], this) || function(){
+				console.warn("Validator '%s' not found. Please use $.Prove.addValidator().", name);
+			};
+			var values = {}; //todo: pass all form values to the validator
+			var isValid = validator(param, value, values);
+			var data = {
+				input: input,
+				validator:{
+					name: name,
+					config: param,
+					state: isValid
+				}
+			};
 
-			//change the scope of this inside the validator
-			validator = $.proxy(validator, this);
-
-			validator(value, element, param);
+			//trigger event indicating validation state
+			if (isValid === true) {
+				this.$form.trigger('valid.field.prove', data);
+			} else if (isValid === false) {
+				this.$form.trigger('invalid.field.prove', data);
+			} else {
+				this.$form.trigger('reset.field.prove', data);
+			}
 		}
 	};
 
@@ -156,15 +193,17 @@
 			var data = el.data('prove');
 			var options = typeof option === 'object' && option;
 			var isInitialized = (data);
+			var Prove = $.Prove;
 
 			// either initialize or call public method
 			if (!isInitialized) {
 				// initialize new instance
 				data = new Prove(this, options);
 				el.data('prove', data);
-				el.trigger('prove.initialized');
+				el.trigger('initialized.prove');
 			} else if (typeof option === 'string') {
 				// call public method
+				// todo: warn if public method does not exist
 				data[option](parameter, extraOptions);
 			} else {
 				throw new Error('invalid invocation.');
@@ -172,6 +211,7 @@
 		});
 	};
 
+	$.Prove = Prove;
 	$.fn.prove.Constructor = Prove;
 
 }(window.jQuery);
