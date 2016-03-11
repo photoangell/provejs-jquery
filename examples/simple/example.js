c7a1c3299b5364d40c2cf0deec0c2ce1f016a101
Dan Hollenbeck <dan@hollenbecks.com>
2016-03-10 20:07:35 -0600

    cleanup of demo

--- examples/simple/example.js
+++ examples/simple/example.js
@@ -7,20 +7,12 @@
 				//trigger: 'blur validate',
 				validators: {
 					proveRequired: {
-						state: '#apple:checked', // true, false, selector string, callback
-						message: 'Your email is required if you has a password.',
+						state: '#optout:checked', // true, false, selector string, callback
+						message: 'Your email is required unless you opt out.',
 					},
 					provePattern: {
 						regex: '[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,63}',
 						message: 'Invalid email address.' //optional, passed to decorator
-					}
-				}
-			},
-			password: {
-				validators: {
-					proveRequired: {
-						state: true, // true, false, selector string, callback
-						message: 'Your password is required.', //optional, passed to decorator
 					}
 				}
 			},
@@ -43,6 +35,15 @@
 		}
 	};
 
+	var events = $('#events');
+	var all = 'validated.field.prove validate.field.prove setup.field.prove destroy.field.prove'
+
+	form.on(all, function(event, data){
+		var input = $(event.target);
+		var ts = new Date(event.timeStamp);
+		events.append('<div>' + event.type + '.' + event.namespace + ': ' + input.attr('name') + ' ' + ts.toISOString() +'</div>');
+	});
+
 	form.on('validated.field.prove', function(event, data){
 		var input = $(event.target);
 		input.bootstrap(data);
@@ -56,6 +57,8 @@
 
 		console.log('submit', ok);
 	});
+
+	form.on()
 
 	//form plugins
 	form.prove(cfg); //validate
