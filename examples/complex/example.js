ec76f2c5d4bc1b609ec015bede9255b5d39b81e2
Daniel Hollenbeck <dan@hollenbecks.com>
2016-03-11 16:38:46 -0600

    complex demo work

--- examples/complex/example.js
+++ examples/complex/example.js
@@ -1,7 +1,7 @@
 (function() {
+
 	var form = $('form');
 	var cfg = {
-		onSubmit: true,
 		fields: {
 			email: {
 				//trigger: 'blur validate',
@@ -83,6 +83,208 @@
 
 	//form plugins
 	form.prove(cfg); //validate
+
+
+		//setup
+		var radio = form.find('[name="statement_attached_has"]');
+
+		var select1 = panel.find('[name="charge_descriptions"]');
+		var monies = form.find('.money');
+		var table = form.find('table#itemization');
+		var wrapper1 = form.find('#wrapper-statement-attached');
+		var wrapper2 = form.find('#wrapper-itemized-amounts');
+		var isItemization;
+
+
+/*		form.validate({
+			rules: {
+				amount_owed: 'required',
+				statement_attached_has: 'required',
+				charge_descriptions: {
+					required: '#use-statement:checked',
+					pattern: '{{patterns.commons.tag.source}}'
+				},
+				itemized_charges_principle_description: {
+					required: '.itemized_charges_principle_description:visible',
+					pattern: '{{patterns.commons.tag.source}}'
+				},
+				itemized_charges_principle_description: {
+					required: '.itemized_charges_principle_description:visible',
+					pattern: '{{patterns.commons.tag.source}}'
+				}
+			},
+			messages: {
+				amount_owed: "Please enter your firstname"
+			},
+			placements: {
+				amount_owed: '.col-sm-4',
+				charge_descriptions: '.placement',
+				statement_attached_has: '.form-group'
+			}
+		});*/
+
+/*		var configTagsinput = {
+			trimValue: true,
+			itemText: toLowerCase,
+			tagClass: tagClass
+		};
+		var configMoneymask = {
+			allowZero: true
+		};
+
+		//tags
+		select1.on('itemAdded', revalidate);
+		select1.on('itemRemoved', revalidate);
+		select1.tagsinput(configTagsinput);
+
+		select1.parent().find('input').addClass('ignore');
+		select1.parent().find('input').attr('name', 'ignore');
+
+		//money mask
+		monies.on('change', revalidate);
+		monies.maskMoney(configMoneymask);*/
+
+		//seup events
+		radio.change(onRadioChange);
+		//table.on('change', 'input.amount', onItemizationChange);
+		//table.on('click', 'a.add-principle-row', addPrincipleChargeRow);
+		//table.on('click', 'a.add-additional-row', addAdditionalChargeRow);
+		//table.on('click', 'a.del-principle-row', delChargeRow);
+		//table.on('click', 'a.del-additional-row', delChargeRow);
+
+
+	//radio wrapper
+	function onRadioChange(event){
+		var val = $(this).val();
+		console.log('onRadioChange()', val);
+		if (val === '1') {
+			isItemization = false;
+			wrapper1.show();
+			wrapper2.hide();
+			resetTable();
+		} else {
+			isItemization = true;
+			wrapper2.show();
+			wrapper1.hide();
+			resetTags();
+		}
+	}
+
+	// ui event handlers and modifiers
+
+	function addPrincipleChargeRow(event){
+		console.log('addPrincipleChargeRow()');
+
+		event.preventDefault();
+		var template = $('#row-principle-charge').text();
+		var target = table.find('tr.active').eq(1);
+		var html = $(template);
+		var input = html.find('input').eq(0);
+		var money = html.find('input').eq(1);
+
+		//insert into dom
+		target.before(html);
+
+		//setup validation
+		//form.formValidation('addField', input);
+
+		//setup money mask
+		money.maskMoney(configMoneymask);
+	}
+
+	function addAdditionalChargeRow(event){
+		console.log('addAdditionalChargeRow()');
+
+		event.preventDefault();
+		var template = $('#row-additional-charge').text();
+		var rows = table.find('tr')
+		var target = rows.eq(rows.length-1);
+		var html = $(template);
+		var input = html.find('input').eq(0);
+		var money = html.find('input').eq(1);
+
+		//insert into dom
+		target.before(html);
+
+		//setup validation
+		//form.formValidation('addField', input);
+
+		//setup money mask
+		money.maskMoney(configMoneymask);
+	}
+
+	function delChargeRow(event){
+		//console.log('delChargeRow()');
+		event.preventDefault();
+		var tr = $(event.target).closest('tr');
+		var money = tr.find('input.money');
+		var input = tr.find('input').eq(0);
+		var money = tr.find('input').eq(1);
+
+		console.log('remove input', input);
+
+		//remove validation
+		//form.formValidation('removeField', input);
+
+		//remove plugins
+		money.maskMoney('destroy');
+
+		tr.remove();
+
+		onItemizationChange();
+	}
+
+
+
+
+
+	function resetTable (){
+		table.find('input.money').val('0.00');
+	}
+
+	function resetTags() {
+		console.log('resetTags()');
+		select1.tagsinput('removeAll');
+		//form.formValidation('resetField', 'charge_descriptions');
+		select1.valid(); //validator.element( "#myselect" );
+		//form.valid();
+	}
+
+	function sumItemizationValues(){
+		//console.log('sumItemizationValues()');
+		var monies = table.find('.amount');
+		var sum = 0;
+		monies.each(function(){
+			var money = $(this).val();
+			var amount = parseFloat(money);
+			sum = sum + amount;
+		});
+		return sum.toFixed(2);
+	}
+
+	function onItemizationChange(){
+		var sum = sumItemizationValues();
+		var summation = table.find('.summation');
+		summation.val(sum);
+	}
+
+
+	function tagClass(item){
+		var isValid = window.patterns.commons.tag.test(item);
+		return (isValid)? 'label label-default' : 'label label-danger';
+	}
+
+	function revalidate(event){
+		console.log('revalidate()', event.target.name);
+		var element = $(event.target);
+		element.valid();
+		//form.formValidation('revalidateField', event.target.name);
+	}
+
+	function toLowerCase(item){
+		return item.toLowerCase();
+	}
+
 
 	//private function
 	function eventRow(event, data){
