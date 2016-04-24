e757a2eb2a84cd825e61b3d5473f7c448ffb82ee
Daniel Hollenbeck <dan@hollenbecks.com>
2016-04-24 15:51:23 -0500

    dirty input bug fix

--- src/utilities/dirty.js
+++ src/utilities/dirty.js
@@ -14,15 +14,34 @@
 		return hash;
 	}
 
+/*	//https://github.com/darkskyapp/string-hash/blob/master/index.js
+	function hashCode2(str) {
+
+		var hash = 5381;
+		var i = str.length;
+
+		while(i) {
+			var code = str.charCodeAt(--i);
+			hash = (hash * 33) ^ code;
+		}
+
+		return hash >>> 0;
+	}*/
+
+
 	$.fn.dirty = function(field) {
 
 		field = field || {};
 
 		var el = $(this);
 		var val = el.val() || '';
-		var hash1 = el.data('prove-hash');
-		var hash2 = hashCode(val);
-		var dirty = (hash1 !== hash2);
+		var hash1, hash2, dirty;
+
+		if ($.isArray(val)) val = val.toString();
+
+		hash1 = el.data('prove-hash');
+		hash2 = hashCode(val);
+		dirty = (hash1 !== hash2);
 
 		// override dirty state for inputs which could be grouped
 		if (field.group) {
