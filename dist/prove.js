331c628584ae8531d8a1c9ab9dece5b420984778
Daniel Hollenbeck <dan@hollenbecks.com>
2016-05-28 13:58:35 -0500

    callback validator

--- dist/prove.js
+++ dist/prove.js
@@ -1038,6 +1038,37 @@
 !function ($) {
 	"use strict";
 
+	$.fn.proveCallback = function(options){
+
+		var input = $(this);
+		var value = input.vals();
+		var enabled = $('body').booleanator(options.enabled);
+		var validated = (enabled && $.isFunction(options.callback) && options.callback())? 'success' : 'danger';
+		var validation = (enabled)? validated : 'reset';
+
+		if (options.debug){
+			console.groupCollapsed('Validator.proveCallback()', options.field);
+				console.log('options', options);
+				console.log('input', input);
+				console.log('value', value);
+				console.log('enabled', enabled);
+				console.log('validation', validation);
+			console.groupEnd();
+		}
+
+		return {
+			field: options.field,
+			validator: options.validator,
+			status: 'validated',
+			validation: validation,
+			message: options.message
+		};
+	};
+}(window.jQuery);
+
+!function ($) {
+	"use strict";
+
 	$.fn.proveCompareTo = function( options ) {
 
 		var input = $(this);
