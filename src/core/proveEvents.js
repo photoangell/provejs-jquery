fcdf0ac62638d25c4bf6f55b313803e24f49acfc
samhollenbeck <sam@hollenbecks.com>
2016-06-08 10:17:53 -0500

    added missing input types

--- src/core/proveEvents.js
+++ src/core/proveEvents.js
@@ -17,14 +17,32 @@
 			return 'change keyup blur';
 		} else if (type === 'password') {
 			return 'change keyup blur';
+		} else if (type === 'hidden') {
+			return 'change';
 		} else if (type === 'radio') {
 			return 'change click blur';
 		} else if (type === 'number') {
 			return 'change keyup blur';
 		} else if (type === 'range') {
 			return 'change keyup click blur';
-		} else if (input.is('select')) {
+		} else if (type === 'button') {
 			return 'change click blur';
+		} else if (type === 'tel') {
+			return 'change keyup blur';
+		} else if (type === 'url') {
+			return 'change keyup blur';
+		} else if (type === 'date') {
+			return 'change keyup blur';
+		} else if (type === 'datetime-local') {
+			return 'change keyup blur';
+		} else if (type === 'month') {
+			return 'change keyup blur';
+		} else if (type === 'time') {
+			return 'change keyup blur';
+		} else if (type === 'week') {
+			return 'change keyup blur';
+		} else if (input.is('select')) {
+			return 'change blur';
 		} else if (input.is('textarea')) {
 			return 'change keyup blur';
 		} else {
