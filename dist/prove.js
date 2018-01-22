9ef80c309b3b658acc9d1c8bb4d1aa00dde2127d
Dan Gorby <GreenRaccoon23@gmail.com>
2018-01-22 13:21:30 -0600

    Fixed regex patterns in sanitize (#11)
    * Fixed regex patterns
    
    * grunt build

--- dist/prove.js
+++ dist/prove.js
@@ -1213,13 +1213,13 @@
 	'use strict';
 
 	function wordToAscii(text) {
-		return text.replace(/[\u2018|\u2019|\u201A]/g, '\'') // smart single quotes and apostrophe
-			.replace(/[\u201C|\u201D|\u201E]/g, '\"') // smart double quotes
+		return text.replace(/(\u2018)|(\u2019)|(\u201A)/g, '\'') // smart single quotes and apostrophe
+			.replace(/(\u201C)|(\u201D)|(\u201E)/g, '\"') // smart double quotes
 			.replace(/\u2026/g, '...') // ellipsis
-			.replace(/[\u2013|\u2014]/g, '-') // dashes
+			.replace(/(\u2013)|(\u2014)/g, '-') // dashes
 			.replace(/\u02C6/g, '^') // circumflex
 			.replace(/\u2039/g, '') // open angle bracket
-			.replace(/[\u02DC|\u00A0]/g, ' '); // spaces
+			.replace(/(\u02DC)|(\u00A0)/g, ' '); // spaces
 	}
 
 	$.fn.sanitize = function() {
